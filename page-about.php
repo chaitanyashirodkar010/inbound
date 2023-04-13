b5335458cfce0e5a6efb9b7375c2e7e531e5e105
chaitanyashirodkar010 <chaitanyashirodkar010@gmail.com>
2023-04-13 17:57:21 +0530

    about us page content ACF fields

--- page-about.php
+++ page-about.php
@@ -19,24 +19,11 @@
             <div class="row">
 
                 <div class="col-lg-6">
-                    <h2 class="ic-font-weight-600">We are Idea Catalyst </h2>
-                    <p>Idea Catalyst is an SEO and thought leadership marketing company. We generate leads for
-                        businesses by ghostwriting content for their website, specializing in complex subject areas. Our
-                        client list includes Yourmechanic.com, Wellness.com and many other Silicon Valley Startups, but
-                        the majority of our clients are small and midsized companies and established startups. Our
-                        customer composition is approximately 65% B2B and 35% B2C.
-                        We begin a campaign by assembling a team that deeply understands your business and customer
-                        base; crafting a strategy for addressing the needs and concerns of your prospects through
-                        content; and producing a regular stream of thought leadership content that produces qualified
-                        leads at a low CAC. We track results carefully and are accountable for exceeding your marketing
-                        benchmarks. </p>
-
-                    <p>We are an excellence-driven company that puts our clients – and their marketing goals – first.
-                    </p>
+                    <?php echo the_field('we_are_idea_catalyst') ?>
                 </div>
                 <div class="col-lg-6">
                     <div class="elvpop">
-                        Elevating people and brands that we believe in.
+                        <?php echo the_field('elvpop') ?>
                     </div>
 
                 </div>
@@ -51,11 +38,8 @@
         <div class="row">
             <div class="col-lg-12">
 
-                <h1 class="ic-font-weight-600 ic-color-black mb-4 text-center">Our mission</h1>
-                <h5 class=" text-center pb-3">At Idea Catalyst, our mission is to provide our clients with
-                    individualized, expert SEO solutions that drive ROI. Our team brings with it a rich and diverse
-                    background in internet marketing, sales, and management, and years of experience as leaders in
-                    search engine optimization space.</h5>
+                <?php echo the_field('our_mission') ?>
+
             </div>
         </div>
     </div>
@@ -67,89 +51,7 @@
 <section class="ic-bnr-sec ic-sec-pad mangeseo pb-0">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="ic-font-weight-600 ic-color-black mb-4 text-center">To achieve our mission, we abide by <span
-                        class="lighthight">cultural values</span> that inform every decision we make and every client
-                    <span class="lighthight">we partner</span> with:</h1>
-
-            </div>
-
-            <div class="col-md-12 ">
-                <div class="ic-spacer-40"></div>
-                <div class="clienpart">
-                    <ul>
-                        <li> We work together as one team, knowing that as a group we are far more powerful than as
-                            individuals, and that together, we can achieve great things.</li>
-
-                        <li>We are proud of our work and excited to celebrate our successes. At the same time, we
-                            welcome feedback that will help us grow and become better.</li>
-
-                        <li>We strive to make each other’s lives easier and better, and consider the effect of all our
-                            actions on our fellow team members.</li>
-
-                        <li>When we feel something is imperfect, we share our feelings in a constructive,
-                            solutions-oriented way so that the people around us have the chance to help us.</li>
-
-                        <li>We are tolerant and accepting of each other, recognizing that we are all human and doing the
-                            best we can every day.</li>
-
-                        <li>We act in a spirit of generosity toward each other.</li>
-
-                    </ul>
-                </div>
-            </div>
-
-
-            <div class="col-lg-12">
-                <div class="ic-spacer-40"></div>
-                <h1 class="ic-font-weight-600 ic-color-black mb-4 text-center">As important as we know words are, it’s
-                    <span class="lighthight">actions</span> that matter.</h1>
-
-            </div>
-            <div class="ic-spacer-40"></div>
-            <div class="col-md-12 pt-5">
-                <div class="clienparts">
-                    <ul>
-                        <li> That’s why at Idea Catalyst, our all-remote team works constantly to build and maintain
-                            connections. This starts with a modern workplace that is welcoming, supportive, and replete
-                            with learning. Every new hire is paired with a mentor who helps show them the ropes, and we
-                            offer company and departmental trainings several times each month.</li>
-
-                        <li>Individual professional development matters here, too. Our team is encouraged and supported
-                            in enhancing their knowledge through trainings and certification courses. We know that the
-                            talent and education of our incredible team translates directly into supporting our clients
-                            with ever-better SEO strategies and creative solutions that help set them apart from
-                            industry competitors.</li>
-
-                        <li>What we do internally is mirrored by our impact externally. We seek out passionate,
-                            cause-oriented clients and provide them with truly impactful thought leadership content
-                            crafted by experts in the field.</li>
-
-                        <li>To retain our talent, we offer benefits including our all-telecommute environment, healthy
-                            work-life balance, health and vision insurance, retirement plan matching, and a robust PTO
-                            package including several additional holidays each year.</li>
-
-                        <li>And we value diversity and inclusion across race, gender, age, religion, identity, and
-                            experience. We have a zero tolerance policy for discrimination and discriminatory language,
-                            a bedrock of our safe and happy work environment. Our team is distributed all across the
-                            United States, and we believe our differences in experience—personal and professional—is
-                            what makes us stronger and richer partners to our clients as well as more creative writers
-                            and problem-solvers.</li>
-
-                        <li>With this foundation of core values, Idea Catalyst strives to give back to our communities.
-                            Our company has thrived on the dedication and excellence of our team and our clients, and we
-                            believe it is essential to carry this forward into the greater community, actively sharing
-                            our knowledge and creativity to help make the internet—and potentially the world—a
-                            better-informed and more positive place. If that sounds good to you, we invite you to join
-                            our team, become our client, or read our blog to learn more.
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-
-
-
+            <?php echo the_field('achieve_our_mission') ?>
         </div>
     </div>
 </section>
@@ -170,19 +72,15 @@
         <div class="talkbox4">
             <div class="row d-flex align-items-center">
                 <div class="col-lg-4">
-                    <img src="<?php echo get_bloginfo('template_directory')?>/images/profileceo.png" width="100%" style="position:relative">
+                    <img src="<?php echo get_bloginfo('template_directory')?>/images/profileceo.png" width="100%"
+                        style="position:relative">
                     <div class="ceopost">
                         <h5>Rishabhh Khurana</h5>
                         <h6>CEO & Head of Sales</h6>
                     </div>
                 </div>
                 <div class="col-lg-6 offset-md-1">
-                    <h2 class="ic-font-weight-600 tittleceo">Founder and CEO </h2>
-
-                    <h6>Idea Catalyst was founded by Rishabh Khurana, one of the early pioneers of SEO. Prior to IDea
-                        Catalyst, he had several exits, including one of the largest travel sites online. </h6>
-                    <h6>Today, Idea Catalyst is the sum of 14+ years of studying Google’s algorithm and the patterns of
-                        effective content. </h6>
+                    <?php echo the_field('founder_content') ?>
                 </div>
 
             </div>
